83b9c8f8dc06caed502773f6c5c3160d0e956484
Nick0576 <nickjohanndejesus0511@gmail.com>
2026-09-12 20:16:09 +0800

    Add pull-to-refresh feature

--- src/routes/_authenticated/u.$username.tsx
+++ src/routes/_authenticated/u.$username.tsx
@@ -107,10 +107,8 @@ function ProfilePage() {
               <div className="relative">
                 <Avatar url={data.profile.avatar_url} name={data.profile.display_name} size={56} />
                 {data.profile.chat_bubble_enabled && data.profile.chat_bubble_text && (
-                  <div className="absolute -top-10 left-1/2 -translate-x-1/2 bg-primary text-primary-foreground text-xs px-3 py-1.5 rounded-2xl shadow-md max-w-32 overflow-hidden">
-                    <div className="overflow-x-auto whitespace-nowrap scrollbar-hide">
-                      {data.profile.chat_bubble_text}
-                    </div>
+                  <div className="absolute -top-10 left-1/2 -translate-x-1/2 bg-primary text-primary-foreground text-xs px-3 py-1.5 rounded-2xl shadow-md whitespace-nowrap">
+                    {data.profile.chat_bubble_text}
                     <div className="absolute -bottom-1 left-1/2 -translate-x-1/2 w-2 h-2 bg-primary rotate-45"></div>
                   </div>
                 )}
