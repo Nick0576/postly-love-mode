8efd873102ebbd707e763b8c9f07b6bc2c55a9e1
Nick0576 <nickjohanndejesus0511@gmail.com>
2026-09-13 23:25:37 +0800

    Fix migration - remove unsupported partial unique constraint

--- supabase/migrations/20260913220000_add_pinned_posts.sql
+++ supabase/migrations/20260913220000_add_pinned_posts.sql
@@ -1,3 +1,1 @@
 ALTER TABLE public.posts ADD COLUMN is_pinned boolean DEFAULT false;
-
-ALTER TABLE public.posts ADD CONSTRAINT posts_pinned_unique UNIQUE (user_id, is_pinned) WHERE is_pinned = true;
