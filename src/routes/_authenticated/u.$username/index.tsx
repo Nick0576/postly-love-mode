e627d44643cb066adeead19c688ba812fa5a1ba1
Nick0576 <nickjohanndejesus0511@gmail.com>
2026-09-13 21:01:02 +0800

    Add missing X icon import from lucide-react

--- src/routes/_authenticated/u.$username/index.tsx
+++ src/routes/_authenticated/u.$username/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+import { X } from "lucide-react";
 import { AppShell } from "@/components/AppShell";
 import { Avatar } from "@/components/Media";
 import { Button } from "@/components/ui/button";
