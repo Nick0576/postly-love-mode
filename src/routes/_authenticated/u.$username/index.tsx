e246105d2d86423eb2a2a37ba112c754b0db6f12
Nick0576 <nickjohanndejesus0511@gmail.com>
2026-09-13 20:17:53 +0800

    Add missing useMutation import

--- src/routes/_authenticated/u.$username/index.tsx
+++ src/routes/_authenticated/u.$username/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { AppShell } from "@/components/AppShell";
 import { Avatar } from "@/components/Media";
